3328e378c5d6393436aadce3e2b9a11fa929d885
Jaime Boardman <jaime@karman.digital>
2023-11-06 15:14:08 +0000

    add update contact api call

---
+++ hubspot/contact.go
@@ -0,0 +1,42 @@
+package hubspot
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/hashicorp/go-retryablehttp"
+	hubspotmodels "github.com/karman-digital/hubspot/hubspot/models"
+)
+
+func (c *credentials) UpdateContact(id int, patchBody hubspotmodels.PatchBody) (hubspotmodels.ContactResponse, error) {
+	var contactResp hubspotmodels.ContactResponse
+	reqUrl := fmt.Sprintf("https://api.hubapi.com/crm/v3/objects/contacts/%d", id)
+	reqBody, err := json.Marshal(patchBody)
+	if err != nil {
+		return contactResp, fmt.Errorf("error marshalling patch body: %s", err)
+	}
+	req, err := retryablehttp.NewRequest("PATCH", reqUrl, reqBody)
+	if err != nil {
+		return contactResp, fmt.Errorf("error creating request: %s", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.AccessToken))
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return contactResp, fmt.Errorf("error making request: %s", err)
+	}
+	defer resp.Body.Close()
+	contactRawBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return contactResp, fmt.Errorf("error reading body: %s", err)
+	}
+	if resp.StatusCode != 200 {
+		return contactResp, fmt.Errorf("error returned by endpoint: %s", contactRawBody)
+	}
+	err = json.Unmarshal(contactRawBody, &contactResp)
+	if err != nil {
+		return contactResp, fmt.Errorf("error parsing body: %s", err)
+	}
+	return contactResp, nil
+}
---
+++ hubspot/models/contact.go
@@ -0,0 +1,11 @@
+package hubspotmodels
+
+import "time"
+
+type ContactResponse struct {
+	Id         string         `json:"id"`
+	Properties map[string]any `json:"properties"`
+	CreatedAt  time.Time      `json:"createdAt"`
+	UpdatedAt  time.Time      `json:"updatedAt"`
+	Archived   bool           `json:"archived"`
+}
