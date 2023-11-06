b717cf8d75dd93ff9d00d66c8ee7feeaa208fe75
Jaime Boardman <jaime@karman.digital>
2023-11-06 15:46:25 +0000

    allow external packages to init hubspot api

---
+++ hubspot/init.go
@@ -0,0 +1,11 @@
+package hubspot
+
+import "github.com/hashicorp/go-retryablehttp"
+
+func InitHubspotAPI() HubspotAPI {
+	client := retryablehttp.NewClient()
+	client.Logger = nil
+	return &credentials{
+		Client: client,
+	}
+}
