312d43b2fcfdd0cde0739db9acd5cf76f948d452
Jaime Boardman <jaime@karman.digital>
2023-12-13 19:19:18 +0000

    add batch update contact function

--- hubspot/api/contact.go
+++ hubspot/api/contact.go
@@ -121,6 +121,46 @@ func (c *credentials) BatchGetContacts(body hubspotmodels.BatchGetBody) (hubspot
 	return contactResp, nil
 }
 
+func (c *credentials) BatchUpdateContacts(body hubspotmodels.BatchUpdateBody) (hubspotmodels.BatchResponse, error) {
+	var contactResp hubspotmodels.BatchResponse
+	reqUrl := "https://api.hubapi.com/crm/v3/objects/contacts/batch/update"
+	reqBody, err := json.Marshal(body)
+	if err != nil {
+		return contactResp, fmt.Errorf("error marshalling post body: %s", err)
+	}
+	req, err := retryablehttp.NewRequest("POST", reqUrl, reqBody)
+	if err != nil {
+		return contactResp, fmt.Errorf("error creating request: %s", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.AccessToken))
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return contactResp, fmt.Errorf("error making request: %s", err)
+	}
+	defer resp.Body.Close()
+	contactRawBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return contactResp, fmt.Errorf("error reading body: %s", err)
+	}
+	if resp.StatusCode != 200 && resp.StatusCode != 207 {
+		var errorResp hubspotmodels.ErrorResponseBody
+		err := json.Unmarshal(contactRawBody, &errorResp)
+		if err != nil {
+			return contactResp, fmt.Errorf("error parsing error body: %s", err)
+		}
+		return contactResp, handleBatchResponseCodes(errorResp, resp.StatusCode)
+	}
+	err = json.Unmarshal(contactRawBody, &contactResp)
+	if err != nil {
+		return contactResp, fmt.Errorf("error parsing body: %s", err)
+	}
+	if resp.StatusCode == 207 {
+		return contactResp, ErrBatchCreate
+	}
+	return contactResp, nil
+}
+
 func (c *credentials) UpdateContact(id int, patchBody hubspotmodels.PatchBody) (hubspotmodels.ContactResponse, error) {
 	var contactResp hubspotmodels.ContactResponse
 	reqUrl := fmt.Sprintf("https://api.hubapi.com/crm/v3/objects/contacts/%d", id)
