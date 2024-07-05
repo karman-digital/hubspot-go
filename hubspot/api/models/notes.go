3611ec1a3dc6ee8d73310136b295870b90903ee0
Jaime Boardman <jaime@karman.digital>
2024-07-05 22:51:03 +0100

    set note properties to omitempty if not required

--- hubspot/api/models/notes.go
+++ hubspot/api/models/notes.go
@@ -8,6 +8,6 @@ type NotePostBody struct {
 type NoteProperties struct {
 	HsTimestamp     int64  `json:"hs_timestamp"`
 	HsNoteBody      string `json:"hs_note_body"`
-	HubspotOwnerId  int    `json:"hubspot_owner_id"`
-	HsAttachmentIds string `json:"hs_attachment_ids"`
+	HubspotOwnerId  int    `json:"hubspot_owner_id,omitempty"`
+	HsAttachmentIds string `json:"hs_attachment_ids,omitempty"`
 }
