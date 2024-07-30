de6a037de393dbe2d21d2c1ff8a6eec4b9df0444
Jaime <jaime@karman.digital>
2024-07-30 16:29:45 +0100

    add associations to generic post Body model

--- hubspot/api/models/post.go
+++ hubspot/api/models/post.go
@@ -1,5 +1,6 @@
 package hubspotmodels
 
 type PostBody struct {
-	Properties Properties `json:"properties"`
+	Properties   Properties                  `json:"properties"`
+	Associations []ObjectCreationAssociation `json:"associations,omitempty"`
 }
