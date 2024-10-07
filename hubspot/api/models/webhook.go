90b9a914b8ffee03d66c53c29801de5b89ec94ab
Jaime Boardman <jaime@karman.digital>
2024-10-07 15:26:07 +0100

    add new generic webhook payload struct to utilise new webhook system

--- hubspot/api/models/webhook.go
+++ hubspot/api/models/webhook.go
@@ -22,3 +22,33 @@ type AccountDataPayload struct {
 	UserEmail  string `json:"userEmail"`
 	AppId      string `json:"appId"`
 }
+
+type GenericPayload struct {
+	ObjectID                int    `json:"objectId"`
+	PropertyName            string `json:"propertyName,omitempty"`
+	PropertyValue           string `json:"propertyValue,omitempty"`
+	ChangeSource            string `json:"changeSource,omitempty"`
+	SubscriptionID          int    `json:"subscriptionId"`
+	SubscriptionType        string `json:"subscriptionType"`
+	EventID                 int64  `json:"eventId"`
+	PortalID                int    `json:"portalId"`
+	AppID                   int    `json:"appId"`
+	OccurredAt              int64  `json:"occurredAt"`
+	AttemptNumber           int    `json:"attemptNumber"`
+	ObjectTypeID            string `json:"objectTypeId"`
+	IsSensitive             bool   `json:"isSensitive,omitempty"`
+	ChangeFlag              string `json:"changeFlag,omitempty"`
+	NewObjectID             int    `json:"newObjectId,omitempty"`
+	PrimaryObjectID         int    `json:"primaryObjectId,omitempty"`
+	MergedObjectIDs         []int  `json:"mergedObjectIds,omitempty"`
+	NumberOfPropertiesMoved int    `json:"numberOfPropertiesMoved,omitempty"`
+	AssociationType         string `json:"associationType,omitempty"`
+	FromObjectID            int    `json:"fromObjectId,omitempty"`
+	ToObjectID              int    `json:"toObjectId,omitempty"`
+	AssociationRemoved      bool   `json:"associationRemoved,omitempty"`
+	IsPrimaryAssociation    bool   `json:"isPrimaryAssociation,omitempty"`
+	AssociationCategory     string `json:"associationCategory,omitempty"`
+	AssociationTypeID       int    `json:"associationTypeId,omitempty"`
+	FromObjectTypeID        string `json:"fromObjectTypeId,omitempty"`
+	ToObjectTypeID          string `json:"toObjectTypeId,omitempty"`
+}
