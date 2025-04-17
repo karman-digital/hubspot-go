bfb2b75e9079b2c41f47890ee0ff6ac59df40078
Jaime Boardman <jaime@karman.digital>
2025-04-17 09:13:53 +0100

    add missing meeting object on engagement init

--- hubspot/api/crm/engagements/init.go
+++ hubspot/api/crm/engagements/init.go
@@ -4,15 +4,17 @@ import (
 	"github.com/karman-digital/hubspot/hubspot/api/credentials"
 	"github.com/karman-digital/hubspot/hubspot/api/crm/engagements/calls"
 	"github.com/karman-digital/hubspot/hubspot/api/crm/engagements/emails"
+	"github.com/karman-digital/hubspot/hubspot/api/crm/engagements/meetings"
 	"github.com/karman-digital/hubspot/hubspot/api/crm/engagements/notes"
 	"github.com/karman-digital/hubspot/hubspot/api/crm/engagements/tasks"
 )
 
 func NewEngagementService(creds *credentials.Credentials) Engagements {
 	return Engagements{
-		Notes:  notes.NewNotesService(creds),
-		Tasks:  tasks.NewTasksService(creds),
-		Calls:  calls.NewCallsService(creds),
-		Emails: emails.NewEmailsService(creds),
+		Notes:    notes.NewNotesService(creds),
+		Tasks:    tasks.NewTasksService(creds),
+		Calls:    calls.NewCallsService(creds),
+		Emails:   emails.NewEmailsService(creds),
+		Meetings: meetings.NewMeetingsService(creds),
 	}
 }
