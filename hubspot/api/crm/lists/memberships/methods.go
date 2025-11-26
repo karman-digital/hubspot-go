04136a945e1d893c83a9e8aa74eb4904a110e519
Jaime Boardman <jaime@karman.digital>
2025-11-26 11:30:51 +0000

    fix get list memberships

--- hubspot/api/crm/lists/memberships/methods.go
+++ hubspot/api/crm/lists/memberships/methods.go
@@ -3,6 +3,7 @@ package memberships
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 
 	listsmodels "github.com/karman-digital/hubspot/hubspot/api/models/crm/lists"
 	sharedmodels "github.com/karman-digital/hubspot/hubspot/api/models/shared"
@@ -11,7 +12,21 @@ import (
 
 func (m *MembershipsService) GetListMemberships(listId string, opts ...sharedmodels.GetOptions) (listsmodels.ListMembershipsResponse, error) {
 	reqUrl := fmt.Sprintf("/crm/v3/lists/%s/memberships", listId)
-	resp, err := m.SendRequest(http.MethodGet, reqUrl, nil, opts...)
+	
+	if len(opts) != 0 {
+		queryParams := url.Values{}
+		if opts[0].After != "" {
+			queryParams.Add("after", opts[0].After)
+		}
+		if opts[0].Limit != 0 {
+			queryParams.Add("limit", fmt.Sprintf("%d", opts[0].Limit))
+		}
+		if encoded := queryParams.Encode(); encoded != "" {
+			reqUrl = fmt.Sprintf("%s?%s", reqUrl, encoded)
+		}
+	}
+	
+	resp, err := m.SendRequest(http.MethodGet, reqUrl, nil)
 	if err != nil {
 		return listsmodels.ListMembershipsResponse{}, fmt.Errorf("error creating request: %s", err)
 	}
